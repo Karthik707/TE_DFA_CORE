d15d25f2d5a8e15b14e204e3d402ca56fc94f528
Karthik707 <karthikkulal707@gmail.com>
2022-10-13 11:46:35 +0530

    commit1

--- DFA_CORE/Models/Employee.cs
+++ DFA_CORE/Models/Employee.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DFA_CORE.Models
 {
     public partial class Employee
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Enter the Trainee Name")]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Name must consist of minimum 3 characters")]
+        [RegularExpression(@"^([A-Za-z]+)$")]
         public string? Name { get; set; }
+        [Required]
         public string? Designation { get; set; }
     }
 }
